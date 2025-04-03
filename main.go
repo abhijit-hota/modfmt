1b5d7c55c1426fd118f500b4d397f13c12e4e435
Alec Sammon <alec.sammon@paddle.com>
2025-04-03 17:37:03 +0100

    * switch if condition

--- main.go
+++ main.go
@@ -22,22 +22,26 @@ func run() error {
 	}
 
 	// check if we want to replace the contents of go.mod
-	if !slices.Contains(os.Args, "--in-place") {
-		// replace not found, so print updated contents to stdout
-		fmt.Println(string(updatedContents))
-		return nil
+	if slices.Contains(os.Args, "--in-place") {
+		return inplace(gomodName, updatedContents)
 	}
 
+	// print updated contents to stdout
+	fmt.Println(string(updatedContents))
+	return nil
+}
+
+func inplace(modLocation string, updatedContents []byte) error {
 	// get current file info
-	info, err := os.Stat(gomodName)
+	info, err := os.Stat(modLocation)
 	if err != nil {
 		return fmt.Errorf("failed to get file info: %w", err)
 	}
 
 	// write updated contents to go.mod
-	if err = os.WriteFile(gomodName, updatedContents, info.Mode()); err != nil {
+	if err = os.WriteFile(modLocation, updatedContents, info.Mode()); err != nil {
 		return fmt.Errorf("failed to write updated go.mod: %w", err)
 	}
-
+	
 	return nil
 }
