fa79289fedb4a530b84799651fd22975a5f992d2
Alec Sammon <alec.sammon@paddle.com>
2025-04-04 07:15:42 +0100

    Add --lint command

--- main.go
+++ main.go
@@ -22,13 +22,27 @@ func run() error {
 	}
 
 	var inplace bool
+	var lint bool
+	var quiet bool
+
 	flag.BoolVar(&inplace, "in-place", false, "replace the contents of go.mod with the updated contents")
 	flag.BoolVar(&inplace, "i", false, "replace the contents of go.mod with the updated contents")
+
+	flag.BoolVar(&lint, "lint", false, "check if the go.mod file is formatted correctly")
+	flag.BoolVar(&lint, "l", false, "check if the go.mod file is formatted correctly")
+
+	flag.BoolVar(&quiet, "quiet", false, "supress output")
+	flag.BoolVar(&quiet, "q", false, "supress output")
+
 	flag.Parse()
 
 	// check if we want to replace the contents of go.mod
 	if inplace {
-		return updateInplace(gomodName, updatedContents)
+		return updateInplace(gomodName, updatedContents, quiet)
+	}
+
+	if lint {
+		return lintContents(gomodName, updatedContents, quiet)
 	}
 
 	// print updated contents to stdout
@@ -36,7 +50,7 @@ func run() error {
 	return nil
 }
 
-func updateInplace(modLocation string, updatedContents []byte) error {
+func updateInplace(modLocation string, updatedContents []byte, quiet bool) error {
 	// get current file info
 	info, err := os.Stat(modLocation)
 	if err != nil {
@@ -46,6 +60,28 @@ func updateInplace(modLocation string, updatedContents []byte) error {
 	// write updated contents to go.mod
 	if err = os.WriteFile(modLocation, updatedContents, info.Mode()); err != nil {
 		return fmt.Errorf("failed to write updated go.mod: %w", err)
+	}
+
+	if !quiet {
+		fmt.Println("go.mod updated successfully")
+	}
+
+	return nil
+}
+
+func lintContents(modLocation string, updatedContents []byte, quiet bool) error {
+	// get current contents of go.mod
+	contents, err := os.ReadFile(modLocation)
+	if err != nil {
+		return fmt.Errorf("failed to read go.mod: %w", err)
+	}
+
+	if string(contents) != string(updatedContents) {
+		return fmt.Errorf("go.mod contents are not formatted correctly")
+	}
+
+	if !quiet {
+		fmt.Println("go.mod is formatted correctly")
 	}
 
 	return nil
