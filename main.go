afa6506f86937f6c3b76e4911d57b769fa4659a0
Alec Sammon <alec.sammon@googlemail.com>
2025-04-05 05:01:26 +0100

    feat: add `--in-place` flag to allow in-place updating (#2)
    * Add `--replace` flag to allow in-place updating
    
    * * switch to --in-place
    
    * * switch if condition
    
    * * use flags package
    
    * * update README
    
    * Update main.go
    
    Co-authored-by: Abhijit Hota <abhihota025@gmail.com>
    
    ---------
    
    Co-authored-by: Abhijit Hota <abhihota025@gmail.com>

--- main.go
+++ main.go
@@ -1,18 +1,51 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"os"
 )
 
 const gomodName = "go.mod"
 
 func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
 
+func run() error {
 	updatedContents, err := MergeRequires(gomodName)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("failed to merge requires: %w", err)
+	}
+
+	var inplace bool
+	flag.BoolVar(&inplace, "in-place", false, "replace the contents of go.mod with the updated contents")
+	flag.BoolVar(&inplace, "i", false, "replace the contents of go.mod with the updated contents")
+	flag.Parse()
+
+	// check if we want to replace the contents of go.mod
+	if inplace {
+		return updateInplace(gomodName, updatedContents)
 	}
 
 	fmt.Println(string(updatedContents))
+	return nil
+}
+
+func updateInplace(modLocation string, updatedContents []byte) error {
+	// get current file info
+	info, err := os.Stat(modLocation)
+	if err != nil {
+		return fmt.Errorf("failed to get file info: %w", err)
+	}
+
+	// write updated contents to go.mod
+	if err = os.WriteFile(modLocation, updatedContents, info.Mode()); err != nil {
+		return fmt.Errorf("failed to write updated go.mod: %w", err)
+	}
+
+	return nil
 }
